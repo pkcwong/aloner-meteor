f9e8f5b3fdbcb1971608e3e9bc26019d31206300
Christopher Wong <christopherpk1996@gmail.com>
2017-11-28 20:54:45 +0800

    enrollment info fetch

--- server/methods/events.js
+++ server/methods/events.js
@@ -37,7 +37,18 @@ export const Events = {
 				packet['location'] = doc.data()['location'];
 				packet['time'] = doc.data()['time'];
 				packet['_id'] = doc.id;
-				resolve(packet);
+				packet['quota'] = doc.data()['quota'];
+				packet['enrollment'] = [];
+				admin.firestore().collection('enrollment').where('event', '==', json['_id']).get().then((snapshot) => {
+					snapshot.forEach((enroll) => {
+						let item = {};
+						item['uid'] = enroll.data()['uid'];
+						item['confirmed'] = enroll.data()['confirmed'];
+						item['approved'] = enroll.data()['approved'];
+						packet['enrollment'].push(item);
+					});
+					resolve(packet);
+				});
 			}).catch((err) => {
 				reject(err);
 			});
@@ -59,6 +70,70 @@ export const Events = {
 					documents.push(packet);
 				});
 				resolve(documents);
+			}).catch((err) => {
+				reject(err);
+			});
+		});
+	},
+
+	create: (json) => {
+		return new Promise((resolve, reject) => {
+			admin.auth().verifyIdToken(json['token']).then((decoded) => {
+				admin.firestore().collection('events').add({
+					category: json['sport'],
+					description: json['description'],
+					location: json['location'],
+					owner: decoded.uid,
+					quota: json['quota'],
+					time: {
+						start: json['time']['start'],
+						end: json['time']['end']
+					}
+				}).then((_id) => {
+					admin.firestore().collection('enrollment').add({
+						approved: true,
+						confirmed: true,
+						event: _id,
+						uid: decoded.uid
+					});
+				}).catch((err) => {
+					reject(err);
+				});
+			}).catch((err) => {
+				reject(err);
+			});
+		});
+	},
+
+	enroll: (json) => {
+		return new Promise((resolve, reject) => {
+			admin.auth().verifyIdToken(json['token']).then((decoded) => {
+				admin.firestore().collection('enrollment').where('event', '==', json['_id']).where('uid', '==', decoded.uid).get().then((snapshot) => {
+					if (snapshot.length  == 0) {
+						admin.firestore().collection('enrollment').add({
+							approved: false,
+							confirm: true,
+							event: json['_id'],
+							uid: decoded.uid
+						}).then(() => {
+							resolve({});
+						}).catch((err) => {
+							reject(err);
+						});
+					} else {
+						snapshot.forEach((doc) => {
+							admin.firestore().collection('enrollment').doc(doc.id).update({
+								confirmed: !doc['confirmed']
+							}).then(() => {
+								resolve({});
+							}).catch((err) => {
+								reject(err);
+							});
+						});
+					}
+				}).catch((err) => {
+					reject(err);
+				});
 			}).catch((err) => {
 				reject(err);
 			});
